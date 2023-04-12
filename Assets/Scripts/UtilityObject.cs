b99f29572fc0d61e75b20a92668e26f93d3818f9
mattcmerritt <mattcmerritt@gmail.com>
2023-04-12 16:46:22 -0400

    objects will now disappear from the scene for all clients when collected

--- Assets/Scripts/UtilityObject.cs
+++ Assets/Scripts/UtilityObject.cs
@@ -18,6 +18,9 @@ public abstract class UtilityObject : SimpleObject
     protected SharedInventory Inventory;
     [SerializeField] public bool Used;
 
+    // Network variable to sync the collection state
+    NetworkVariable<bool> IsCollected = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
     protected override void Start()
     {
         base.Start();
@@ -31,23 +34,36 @@ public abstract class UtilityObject : SimpleObject
         ChildMeshes = GetComponentsInChildren<MeshRenderer>();
     }
 
+    public override void OnNetworkSpawn()
+    {
+        IsCollected.OnValueChanged += (bool previousValue, bool newValue) =>
+        {
+            // removing the object when it is collected
+            if (newValue == true)
+            {
+                // cannot simply destroy the object, as the behavior script needs to remain in the scene
+                if (MeshRenderer != null)
+                {
+                    MeshRenderer.enabled = false;
+                }
+                else if (ChildMeshes != null)
+                {
+                    foreach (MeshRenderer mesh in ChildMeshes)
+                    {
+                        mesh.enabled = false;
+                    }
+                }
+                Collider.enabled = false;
+            }
+        };
+    }
+
     public void Collect()
     {
         Inventory.AddItem(this);
-        // cannot simply disable the object, as the script to control the UI
-        // will stop working and listening for keys
-        if (MeshRenderer != null)
-        {
-            MeshRenderer.enabled = false;
-        }
-        else if (ChildMeshes != null)
-        {
-            foreach (MeshRenderer mesh in ChildMeshes)
-            {
-                mesh.enabled = false;
-            }
-        }
-        Collider.enabled = false;
+
+        // Collect the object for all clients
+        CollectServerRpc();
     }
 
     // This method will be the behavior that will need to be executed for all clients when used
@@ -55,5 +71,11 @@ public abstract class UtilityObject : SimpleObject
     public virtual void InteractAllClients(PlayerInteractions player)
     {
         // This method does not currently do anything by default
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void CollectServerRpc()
+    {
+        IsCollected.Value = true;
     }
 }
