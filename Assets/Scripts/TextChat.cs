e20cc0096898bfd6b681877b6755fd2fa0005399
mattcmerritt <mattcmerritt@gmail.com>
2023-09-05 11:29:05 -0400

    timestamps are now generated when message is sent

--- Assets/Scripts/TextChat.cs
+++ Assets/Scripts/TextChat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,13 @@ using Unity.Netcode;
 public struct ChatMessage
 {
     public string PlayerName;
-    public string TimeStamp;
+    public string Timestamp;
     public string Message;
 
-    public ChatMessage(string playerName, string timeStamp, string message)
+    public ChatMessage(string playerName, string timestamp, string message)
     {
         PlayerName = playerName;
-        TimeStamp = timeStamp;
+        Timestamp = timestamp;
         Message = message;
     }
 }
@@ -23,23 +24,39 @@ public class TextChat : NetworkBehaviour
     // Chat history
     [SerializeField] private List<ChatMessage> ChatHistory = new List<ChatMessage>();
 
+    // TODO: remove testing code
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) 
+        {
+            SendMessage("Player", "Test Message");
+        }
+    }
+
+
     // TODO: implement player name and timestamp in a way that does not require it as a parameter
     //       this can be done using instance data or by reading it from other places
-    public void SendMessage(string playerName, string timeStamp, string message)
+    public void SendMessage(string playerName, string message)
     {
-        AddChatMessageForAllServerRpc(playerName, timeStamp, message);
+        // fetching the current time
+        DateTime currentTime = DateTime.Now;
+        string timestamp = currentTime.ToString("HH:mm");
+
+        Debug.Log($"Time of message: {timestamp}");
+
+        AddChatMessageForAllServerRpc(playerName, timestamp, message);
     }
 
     [ServerRpc(RequireOwnership = false)]
-    private void AddChatMessageForAllServerRpc(string playerName, string timeStamp, string message)
+    private void AddChatMessageForAllServerRpc(string playerName, string timestamp, string message)
     {
-        AddChatMessageClientRpc(playerName, timeStamp, message);
+        AddChatMessageClientRpc(playerName, timestamp, message);
     }
 
     [ClientRpc] 
-    private void AddChatMessageClientRpc(string playerName, string timeStamp, string message)
+    private void AddChatMessageClientRpc(string playerName, string timestamp, string message)
     {
-        ChatMessage newMessage = new ChatMessage(playerName, timeStamp, message);
+        ChatMessage newMessage = new ChatMessage(playerName, timestamp, message);
         ChatHistory.Add(newMessage);
     }
 }
