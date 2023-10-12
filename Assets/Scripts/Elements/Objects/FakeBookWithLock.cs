6deee9ff767f47ef4a2ee8a9e606068ffc2b69e2
mikepmerritt <mikepmerritt@gmail.com>
2023-10-12 09:07:38 -0400

    added other fake book with clue 5 and secret paper

---
+++ Assets/Scripts/Elements/Objects/FakeBookWithLock.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeBookWithLock : DraggableObject
+{
+    [SerializeField] private MeshRenderer KeyHover;
+    private bool FirstTimeSetup;
+    private SharedInventory Inventory;
+    [SerializeField] private UtilityObject ItemInBook;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        FirstTimeSetup = true;
+
+        Inventory = FindObjectOfType<SharedInventory>();
+    }
+
+    protected override void Update() {
+        base.Update();
+
+        if(FirstTimeSetup && IsCopy) {
+            // KeyHover.enabled = true;
+            FirstTimeSetup = false;
+        }
+
+        UtilityObject keyItem = Inventory.CheckForItem("Key");
+        if(IsCopy && keyItem != null)
+        {
+            FakeBookKey key = (FakeBookKey) keyItem;
+            if(Input.GetMouseButtonDown(1)) 
+            {
+                // TODO: play a hinge opening animation
+                Inventory.AddItem(ItemInBook);
+
+                UIManager manager = FindObjectOfType<UIManager>();
+                manager.ShowPopupPanel(ItemInBook.ItemDetails.Name, ItemInBook.ItemDetails.Icon);
+            }
+        }
+    }
+}
