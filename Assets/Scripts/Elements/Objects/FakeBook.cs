e976c695ceb3500b52e07b78726630ed4049819f
mikepmerritt <mikepmerritt@gmail.com>
2023-10-12 07:43:55 -0400

    when key is picked up, right clicking in book window gives clue 5

--- Assets/Scripts/Elements/Objects/FakeBook.cs
+++ Assets/Scripts/Elements/Objects/FakeBook.cs
@@ -6,19 +6,40 @@ public class FakeBook : DraggableObject
 {
     [SerializeField] private MeshRenderer KeyHover;
     private bool FirstTimeSetup;
+    private SharedInventory Inventory;
+    [SerializeField] private UtilityObject ItemInBook;
 
     protected override void Start()
     {
         base.Start();
 
         FirstTimeSetup = true;
+
+        Inventory = FindObjectOfType<SharedInventory>();
     }
 
     protected override void Update() {
         base.Update();
+
         if(FirstTimeSetup && IsCopy) {
-            KeyHover.enabled = true;
+            // KeyHover.enabled = true;
             FirstTimeSetup = false;
+        }
+
+        UtilityObject keyItem = Inventory.CheckForItem("Key");
+        if(IsCopy && keyItem != null)
+        {
+            FakeBookKey key = (FakeBookKey) keyItem;
+            if(Input.GetMouseButtonDown(1)) 
+            {
+                // TODO: play a hinge opening animation
+                Inventory.AddItem(ItemInBook);
+
+                UIManager manager = FindObjectOfType<UIManager>();
+                manager.ShowPopupPanel(ItemInBook.ItemDetails.Name, ItemInBook.ItemDetails.Icon);
+
+                Debug.Log("Click with key");
+            }
         }
     }
 }
