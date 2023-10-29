239f8e1c275dc83b7310f842359d827237a99d7c
mikepmerritt <mikepmerritt@gmail.com>
2023-10-28 22:55:31 -0400

    fix uv pen for clients

--- Assets/Scripts/Elements/Objects/UVPen.cs
+++ Assets/Scripts/Elements/Objects/UVPen.cs
@@ -5,19 +5,32 @@ using Unity.Netcode;
 
 public class UVPen : UtilityObject
 {
-    private NetworkVariable<bool> LightActive = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Everyone);
+    private bool LightActive;
     [SerializeField] private Color DefaultPanelColor, LightOnPanelColor;
 
     // This item will not be usable in the main game world
     public override void InteractAllClients(PlayerInteractions player)
     {
         base.InteractAllClients(player);
-        LightActive.Value = !LightActive.Value;
+        LightActive = !LightActive;
+        ChangeValueForAllServerRpc(LightActive);
         // player.ChangePanelColor("3D Object", LightActive.Value ? LightOnPanelColor : DefaultPanelColor);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ChangeValueForAllServerRpc(bool value)
+    {
+        ChangeValueClientRpc(value);
+    }
+
+    [ClientRpc]
+    private void ChangeValueClientRpc(bool value)
+    {
+        LightActive = value;
     }
 
     public bool CheckLight()
     {
-        return LightActive.Value;
+        return LightActive;
     }
 }
