a81bfb38d268a35ec891447944106f62dc024e63
mattcmerritt <mattcmerritt@gmail.com>
2023-11-18 16:44:27 -0500

    allowed player to reset the scrabble board with r key

--- Assets/Scripts/UI/Objects/ScrabbleTile.cs
+++ Assets/Scripts/UI/Objects/ScrabbleTile.cs
@@ -5,6 +5,23 @@ using UnityEngine.EventSystems;
 
 public class ScrabbleTile : MonoBehaviour, IDragHandler
 {
+    private RectTransform RectTransform;
+    private Vector3 InitialPosition;
+
+    private void Start()
+    {
+        RectTransform = GetComponent<RectTransform>();
+        InitialPosition = RectTransform.localPosition;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RectTransform.localPosition = InitialPosition;
+        }
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         Vector3 globalMousePos;
