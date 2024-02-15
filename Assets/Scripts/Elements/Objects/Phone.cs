9b7214187f0f0b633680600c932e884c75600102
mattcmerritt <mattcmerritt@gmail.com>
2024-02-15 00:32:54 -0500

    phones now open ui for all players

--- Assets/Scripts/Elements/Objects/Phone.cs
+++ Assets/Scripts/Elements/Objects/Phone.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class Phone : SimpleObject
 {
-    // Just opens the phone chat screen
+    // Phone launches the phone UI for all players
+    public override void Interact(PlayerInteractions player)
+    {
+        if (PanelID != "None")
+        {
+            OpenUIForAllServerRpc();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void OpenUIForAllServerRpc()
+    {
+        OpenUIForAllClientRpc();
+    }
+
+    [ClientRpc]
+    private void OpenUIForAllClientRpc() 
+    {
+        PlayerInteractions player = FindObjectOfType<PlayerInteractions>();
+        player.OpenWithUIManager(PanelID);
+    }
 }
