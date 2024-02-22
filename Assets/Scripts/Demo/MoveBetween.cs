7ae7a375017e4e6f2511051a30b8cb95adb6b129
mattcmerritt <mattcmerritt@gmail.com>
2024-02-22 05:41:10 -0500

    added demonstration of moving cahracter in and out of raycast distance

---
+++ Assets/Scripts/Demo/MoveBetween.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetween : MonoBehaviour
+{
+    [SerializeField] private Vector3 positionA, positionB;
+    private float transitionDuration = 1;
+    private Coroutine activeCoroutine;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Forward");
+            if (activeCoroutine != null)
+            {
+                StopCoroutine(activeCoroutine);
+            }
+            activeCoroutine = StartCoroutine(MoveToPoint(positionA, positionB));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Backwards");
+            if (activeCoroutine != null)
+            {
+                StopCoroutine(activeCoroutine);
+            }
+            activeCoroutine = StartCoroutine(MoveToPoint(positionB, positionA));
+        }
+    }
+
+    private IEnumerator MoveToPoint(Vector3 start, Vector3 target)
+    {
+        // transition time depends on how far the previous transition was
+        float timeElapsed = (transform.position - start).magnitude / (target - start).magnitude * transitionDuration;
+
+        while (timeElapsed < transitionDuration)
+        {
+            transform.position = new Vector3(
+                Mathf.Lerp(start.x, target.x, timeElapsed / transitionDuration),
+                Mathf.Lerp(start.y, target.y, timeElapsed / transitionDuration),
+                Mathf.Lerp(start.z, target.z, timeElapsed / transitionDuration)
+            );
+
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/Player/HideOnProximity.cs
+++ Assets/Scripts/Player/HideOnProximity.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 
 public class HideOnProximity : MonoBehaviour
 {
+    // internal fade in/out data
     private List<MeshRenderer> renderers;
     private List<Color> originalColors;
     private float hiddenOpacity = 0.33f;
     private float transitionDuration = 1;
-    private bool hidden;
+    private float fadeInDelay = 0.5f;
+    private bool isHiding;
+    [SerializeField] private bool modelHidden;
     private List<Coroutine> activeCoroutines;
+    private Coroutine fadeInDelayCoroutine;
+
+    // camera and raycasting
+    [SerializeField] private GameObject cameraObject;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float fadeOutDistance;
 
     private void Start()
     {
@@ -26,7 +35,19 @@ public class HideOnProximity : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        RaycastHit hit;
+        if (Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, fadeOutDistance, playerLayer))
+        {
+            Debug.Log($"Hit {hit.collider.name}");
+            HideOnProximity hider = hit.collider.gameObject.GetComponent<HideOnProximity>();
+            hider.MarkAsHidden();
+        }
+    }
+
+    public void MarkAsHidden()
+    {
+        // stop any fade in coroutines
+        if (!isHiding)
         {
             while (activeCoroutines.Count > 0)
             {
@@ -40,28 +61,62 @@ public class HideOnProximity : MonoBehaviour
                     activeCoroutines.RemoveAt(0);
                 }
             }
-            
-            if (!hidden)
+        }
+
+        // start fade out coroutines if not active
+        if (!isHiding)
+        {
+            Debug.Log("Unhiding!");
+            for (int i = 0; i < renderers.Count; i++)
             {
-                for (int i = 0; i < renderers.Count; i++)
-                {
-                    activeCoroutines.Add(StartCoroutine(FadeOut(i)));
-                    hidden = true;
-                }
+                activeCoroutines.Add(StartCoroutine(FadeOut(i)));
             }
-            else if (hidden)
+            fadeInDelayCoroutine = StartCoroutine(WaitForFadeIn());
+            isHiding = true;
+        }
+        // else reset fade in delay
+        else
+        {
+            Debug.Log("Continue waiting!");
+            if (fadeInDelayCoroutine != null)
             {
-                for (int i = 0; i < renderers.Count; i++)
-                {
-                    activeCoroutines.Add(StartCoroutine(FadeIn(i)));
-                    hidden = false;
-                }
+                StopCoroutine(fadeInDelayCoroutine);
+                fadeInDelayCoroutine = StartCoroutine(WaitForFadeIn());
             }
         }
     }
 
+    public IEnumerator WaitForFadeIn()
+    {
+        // wait until it has been sufficiently long without player blocking
+        yield return new WaitForSeconds(fadeInDelay);
+
+        // stop any fade out coroutines
+        while (activeCoroutines.Count > 0)
+        {
+            if (activeCoroutines[0] != null)
+            {
+                StopCoroutine(activeCoroutines[0]);
+                activeCoroutines.RemoveAt(0);
+            }
+            else
+            {
+                activeCoroutines.RemoveAt(0);
+            }
+        }
+
+        // starting fade in coroutines
+        for (int i = 0; i < renderers.Count; i++)
+        {
+            activeCoroutines.Add(StartCoroutine(FadeIn(i)));
+        }
+        isHiding = false;
+    }
+
     public IEnumerator FadeOut(int i)
     {
+        Debug.Log("Fading out!");
+
         renderers[i].material.SetInt("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.SrcAlpha);
         renderers[i].material.SetInt("_DstBlend", (int) UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
         renderers[i].material.SetInt("_ZWrite", 0);
@@ -88,10 +143,14 @@ public class HideOnProximity : MonoBehaviour
                 yield return null;
             }
         }
+
+        modelHidden = true;
     }
 
     public IEnumerator FadeIn(int i)
     {
+        Debug.Log("Fading in!");
+
         if (renderers[i].material.HasProperty("_Color"))
         {
             // transition time depends on how far the previous transition was
@@ -118,5 +177,7 @@ public class HideOnProximity : MonoBehaviour
         //renderers[i].material.DisableKeyword("_ALPHAPREMULTIPLY_ON"); // enable for fade
         renderers[i].material.DisableKeyword("_ALPHABLEND_ON"); // enable for transparent
         renderers[i].material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
+
+        modelHidden = false;
     }
 }
