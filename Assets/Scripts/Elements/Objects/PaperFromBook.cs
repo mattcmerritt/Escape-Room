81c2020015449cf8bd3752b892c9357ee59a32b6
mattcmerritt <mattcmerritt@gmail.com>
2024-03-21 05:43:54 -0400

    clue 7 is no longer stored in the locked book, added new paper instead

--- Assets/Scripts/Elements/Objects/PaperFromBook.cs
+++ Assets/Scripts/Elements/Objects/PaperFromBook.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PaperFromBook : UtilityObject
 {
+    [SerializeField] private string PanelName = "Paper";
+
     public override void Interact(PlayerInteractions player) 
     {
         UIManager manager = FindObjectOfType<UIManager>();
-        manager.OpenUI("Paper");
+        manager.OpenUI(PanelName);
     }
 }
