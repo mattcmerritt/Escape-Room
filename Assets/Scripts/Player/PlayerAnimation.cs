1898924ccedfb1e093e3ca37af5b44fb31215d3d
mattcmerritt <mattcmerritt@gmail.com>
2024-03-28 04:29:02 -0400

    updated animation variable visibility

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -6,7 +6,7 @@ using Unity.Netcode;
 public class PlayerAnimation : NetworkBehaviour
 {
     [SerializeField] private Animator ModelAnimator;
-    private NetworkVariable<bool> PlayerIsMoving = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<bool> PlayerIsMoving = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     public override void OnNetworkSpawn()
     {
