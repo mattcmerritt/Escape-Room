f2436debc3ed53dac037b53869544b7cb85da55f
mattcmerritt <mattcmerritt@gmail.com>
2024-04-02 05:46:01 -0400

    popups should be networked for inventory

--- Assets/Scripts/UI/Game/PopupUI.cs
+++ Assets/Scripts/UI/Game/PopupUI.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Unity.Netcode;
 
-public class PopupUI : MonoBehaviour
+[System.Serializable]
+public class PopupInformation
+{
+    public string itemName;
+    public Sprite itemImage;
+}
+
+public class PopupUI : NetworkBehaviour
 {
     [SerializeField] private TMP_Text TitleText, DescriptionText;
     [SerializeField] private Image ItemIcon;
@@ -12,10 +20,40 @@ public class PopupUI : MonoBehaviour
     // Animations
     [SerializeField] private Animator Animator;
 
+    // Popups
+    [SerializeField] private List<PopupInformation> PopupInformationList;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        PopupInformationList = new List<PopupInformation>();
+        InventoryItem[] items = Resources.LoadAll<InventoryItem>("Scriptable Objects");
+        foreach (InventoryItem item in items)
+        {
+            PopupInformationList.Add(new PopupInformation { itemName = item.Name, itemImage = item.Icon });
+        }
+        PopupInformationList.Sort((PopupInformation popup1, PopupInformation popup2) => popup1.itemName.CompareTo(popup2.itemName));
+    }
+
     public void ShowPopup(string itemName, Sprite itemImage)
     {
-        TitleText.text = itemName;
-        ItemIcon.sprite = itemImage;
+        int index = PopupInformationList.FindIndex((PopupInformation p) => itemName == p.itemName && itemImage == p.itemImage);
+        // Debug.Log($"Opening {itemName} with image {itemImage}, found at {index}");
+        ShowPopupServerRpc(index);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void ShowPopupServerRpc(int index)
+    {
+        ShowPopupClientRpc(index);
+    }
+
+    [ClientRpc]
+    public void ShowPopupClientRpc(int index)
+    {
+        TitleText.text = PopupInformationList[index].itemName;
+        ItemIcon.sprite = PopupInformationList[index].itemImage;
 
         Animator.SetTrigger("Show");
     }
