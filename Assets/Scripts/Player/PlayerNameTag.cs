46fa0d4784307eb48dda1a7254ec8610d15c5bbf
mikepmerritt <mikepmerritt@gmail.com>
2024-04-09 01:32:13 -0400

    modified nametag rotation to now look at active player

--- Assets/Scripts/Player/PlayerNameTag.cs
+++ Assets/Scripts/Player/PlayerNameTag.cs
@@ -10,7 +10,8 @@ public class PlayerNameTag : NetworkBehaviour
     // name stuff
     private NetworkVariable<FixedString128Bytes> Name = new NetworkVariable<FixedString128Bytes>("Unnamed Player", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     [SerializeField] private GameObject NameTag;
-
+    [SerializeField] private TMP_Text NameTagText;
+    [SerializeField] private GameObject PlayerToLookAt;
 
     public override void OnNetworkSpawn()
     {
@@ -20,6 +21,18 @@ public class PlayerNameTag : NetworkBehaviour
         {
             Name.Value = FindObjectOfType<PlayerClientData>().GetPlayerName();
         }
+
+        if(!IsOwner)
+        {
+            PlayerNameTag[] PlayerList = FindObjectsOfType<PlayerNameTag>();	
+            foreach (PlayerNameTag player in PlayerList)	
+            {	
+                if (player.gameObject.GetComponent<NetworkObject>().IsLocalPlayer)	
+                {	
+                    PlayerToLookAt = player.gameObject;	
+                }	
+            }
+        }
     }
 
     private void Update()
@@ -27,12 +40,12 @@ public class PlayerNameTag : NetworkBehaviour
         if(!IsOwner)
         {
             SetNameTag(Name.Value);
-            NameTag.transform.LookAt(transform);
+            NameTag.transform.LookAt(PlayerToLookAt.transform);
         }
     }
 
     public void SetNameTag(FixedString128Bytes name)
     {
-        NameTag.GetComponent<TMP_Text>().text = "" + name.Value;
+        NameTagText.text = "" + name.Value;
     }
 }
